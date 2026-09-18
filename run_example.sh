abd29b0021dca9105be4ae8f86942bdc2e132b1b
Illia Oleksiienko <io@ece.au.dk>
2026-09-18 18:45:02 +0000

    Update readme

--- run_example.sh
+++ run_example.sh
@@ -2,4 +2,4 @@ mkdir -p ../results/single_runs
 ln -s ../results
 ln -s ../results/single_runs
 
-python ./enn/experiments/neurips_2021/run_testbed_best_selected.py --input_dim=10 --data_ratio=1 --noise_std=0.1 --agent_id_start=3 --agent_id_end=5 --agent=true_layer_ensemble_einsum_cor --seed=2605
+PYTHONPATH=. python ./src/experiments/run_testbed_discrete_bnn_parallel.py --input_dim=10 --data_ratio=1 --noise_std=0.1 --agent_name=vnn_best --processes_per_device="${1:-1}" --devices="${2:-7}" --results_folder=results-discrete --seeds=[2605]
